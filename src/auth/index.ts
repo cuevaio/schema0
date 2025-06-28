4385213e25fb9d699c94b6af09aefabea3cbfb4e
Anthony Cueva <hi@cueva.io>
2025-06-28 15:02:25 -0500

    feat: initial setup

---
+++ src/auth/index.ts
@@ -0,0 +1,12 @@
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+import { db } from "../database";
+import * as schema from "../database/schema";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema,
+  }),
+});
